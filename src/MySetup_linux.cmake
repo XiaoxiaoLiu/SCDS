46f107cfbbaceb077c241f205a1b27f752815545
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2011-11-20 19:33:52 -0500

    ENH:  Build against ITK3.20.
    Test the toolkit build against ITK3.20.

--- src/MySetup_linux.cmake
+++
@@ -1,9 +0,0 @@
-#SET( VAR ¡°value¡± CACHE TYPE ¡°description¡± FORCE )
-
-SET(FFTWF_THREADS_LIB "/afs/radonc.unc.edu/home/sharonxx/public/libraries/fftw/lib/libfftw3f_threads.a" CACHE FILEPATH "xx" FORCE)
-SET(FFTW_INCLUDE_PATH "/afs/radonc.unc.edu/home/sharonxx/public/libraries/fftw/include" CACHE PATH  "xx" FORCE)
-SET(ITK_DIR  "/afs/radonc.unc.edu/home/sharonxx/public/libraries/ITK" CACHE PATH "xx" FORCE)
-SET(LAPACK_BASE_DIR  "/afs/radonc.unc.edu/home/sharonxx/public/libraries/CLAPACK" CACHE PATH "xx" FORCE)                             
-SET(RADONC_LIBS_PATH  "/afs/radonc.unc.edu/home/sharonxx/public/libraries" CACHE PATH "xx" FORCE)     
-SET(VTK_DIR "/afs/radonc.unc.edu/home/sharonxx/public/libraries/VTK" CACHE PATH "xx" FORCE)
-SET(FFTWF_LIB  "/afs/radonc.unc.edu/home/sharonxx/public/libraries/fftw/lib/libfftw3f.a" CACHE FILEPATH "xx" FORCE)
